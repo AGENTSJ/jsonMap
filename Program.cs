c055a37a4ab673b1b345cb7fa0e8b72264cedf1b
Agentsj <abhijithsj4@gmail.com>
2024-07-21 11:31:14 +0530

    array parser complete

--- Program.cs
+++ Program.cs
@@ -13,12 +13,13 @@ namespace jsonMap
         static void Main(string[] args)
         {
            
-            string jsonTest = "{\"name\":  \"Abhijith SJ\",\"numericDetails\":{\"age\": 21,\"rollNo\": 2},\"randomArr\":[1,2,3,4]}";
+            string jsonTest = "{\"name\":  \"Abhijith SJ\",\"numericDetails\":{\"age\": 21,\"rollNo\": 2},\"randomArr\":[1,2,3,4],\"testarr\":[{\"age\":18,\"rollNo\":14}]}";
             // Test temper = JsonMap.jsonMap<Test>(jsonTest);
             // Console.WriteLine(temper.numericDetails.age);
             JsonMap jMap = new();
             Test test = jMap.Extract<Test>(jsonTest);
             Console.WriteLine(test.numericDetails.rollNo);
+            Console.WriteLine(test.testarr[0].age);
         }
     }
     class Test
@@ -26,6 +27,8 @@ namespace jsonMap
         public string name;
         public test2 numericDetails = new test2();
         public int[] randomArr;
+
+        public test2[] testarr = new test2[5];
     }
     class test2
     {
--- jsonMap.cs
+++ jsonMap.cs
@@ -1,72 +1,108 @@
 using System.Reflection;
-using System.Linq;
-namespace jsonMap{
+using System.Collections;
+namespace jsonMap
+{
 
-    class JsonMap {
+    class JsonMap
+    {
         private int idx = 0;
+
+        /* returns a class that is poppulated with contents from json String */
         public T Extract<T>(string jsonString) where T : new()
         {
-            /*
-                returns a class that is poppulated with contents from json String
-            */
             T result = new T();
             Dictionary<string, object> contentMap;
 
             contentMap = JsonParse(jsonString);//parser
             Maper(result, contentMap);//maper
             return result;
-        } 
+        }
+        
+        /* Maps fields or attributes of a input instance to their corresponding value that is returned from jsonParser */
         private void Maper(object? resultInstance, Dictionary<string, object> contentMap)
         {
-            /*
-                Maps fields or attributes of a given instance to their corresponding value that is returned from jsonParser
-            */
+           
+            const string STRING = "string";
+            const string INT = "int32";
+            const string ARRAY_IDEN = "[]";
+
             Type typeOfInstance;
             FieldInfo[] attributes;
-            if(resultInstance== null){
+            if (resultInstance == null)
+            {
                 return;
             }
+
             typeOfInstance = resultInstance.GetType();
             attributes = typeOfInstance.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
 
+            string varTypeFullName;
             string varTypeName;
             string varName;
             object value;
-            foreach (FieldInfo attr in attributes)
+
+            foreach (FieldInfo attr in attributes) // set each attribute of a class
             {
+                varTypeFullName = attr.FieldType.FullName;
                 varTypeName = attr.FieldType.Name.ToLower();
                 varName = attr.Name;
                 value = contentMap[varName];
-                
-                if (varTypeName == "string")
+
+                if (varTypeName == STRING)
                 {
                     attr.SetValue(resultInstance, (string)value);
+
                 }
-                else if (varTypeName == "int32[]")
+
+                else if (varTypeName.Contains(ARRAY_IDEN))
                 {
-                    // Console.WriteLine(string.Join(",",value)+ value.GetType());
-                    object[]temp = (object[])value;
-                    // attr.SetValue(resultInstance, value);
-                    foreach(object ob in temp){
-                        Console.WriteLine(ob);
+                    object[] temp = (object[])value;
+                    Type arrayElementType = attr.FieldType.GetElementType();
+                    var listType = typeof(List<>).MakeGenericType(arrayElementType);
+                    var list = (IList)Activator.CreateInstance(listType);
+
+                    // Console.WriteLine(arrayElementType);
+                    foreach (object ob in temp)
+                    {
+                        if (!ob.GetType().Name.ToLower().Contains("dictionary"))
+                        {
+                            var castedValue = Convert.ChangeType(ob, arrayElementType);
+                            list.Add(castedValue);
+                        }
+                        else
+                        {
+                            object nestedClassInstance = Activator.CreateInstance(arrayElementType);
+                            Maper(nestedClassInstance,(Dictionary<string,object>)ob);
+                            list.Add(nestedClassInstance);
+                        }
+                        var toArrayMethod = list.GetType().GetMethod("ToArray");
+                        var array = toArrayMethod.Invoke(list, null);
+                        attr.SetValue(resultInstance, toArrayMethod.Invoke(list, null));
+                        
                     }
+                    
+
+                    
+
                 }
-                else if (varTypeName == "int"|| varTypeName == "int32")
+
+                else if (varTypeName == INT || varTypeName == "int")
                 {
                     attr.SetValue(resultInstance, (int)value);
+
                 }
-                else
+
+                else if (attr.GetValue(resultInstance) != null)
                 {
-                    if(attr.GetValue(resultInstance)!=null){
-                        Maper(attr.GetValue(resultInstance), (Dictionary<string ,object>)value);
-                    }   
+                    Maper(attr.GetValue(resultInstance), (Dictionary<string, object>)value);
                 }
             }
             return;
         }
+       
+        /*Parses a json string and creates a hashmap that contain attribute Name -> attribute value*/
         private Dictionary<string, object> JsonParse(string json)
         {
-            /*Parses a json string and creates a hashmap that contain attribute Name -> attribute value*/
 
             Dictionary<string, object> attributes = new Dictionary<string, object>();
 
@@ -86,12 +122,10 @@ namespace jsonMap{
             idx++; //skipping -> }
             return attributes;
         }
+        
+        /* reads and return the attribute name or key */
         private string ReadKey(string json)
         {
-            /*
-                reads and return the attribute name or key 
-            */
-
             string buffer = "";
             SkipWhitespaces(json);
             if (json[idx] == ',')
@@ -116,10 +150,10 @@ namespace jsonMap{
             idx++; //skiping -> :
             return buffer;
         }
+        
+        /*Read attribute value from json string and returns*/
         private object ReadValue(string json)
         {
-            /*Read attribute value from json string and returns*/
-            
             SkipWhitespaces(json);
 
             if (json[idx] == '{')
@@ -147,9 +181,10 @@ namespace jsonMap{
             }
 
         }
+        
+        /*reads integer that is in string format and returns*/
         private int ParseInt(string json)
         {
-            /*reads integer that is in string format and returns*/
             string buffer = "";
             int res = 0;
             while ("1234567890".Contains(json[idx]) && idx < json.Length)
@@ -160,10 +195,10 @@ namespace jsonMap{
             int.TryParse(buffer, out res);
             return res;
         }
+        
+        // parses array from with in [ -> ]
         private object[] ParseArray(string json)
         {
-            // parses array from with in [ -> ]
-            
             if (json[idx] != '[')
             {
                 Console.WriteLine("invalid start of an array ");
@@ -177,17 +212,20 @@ namespace jsonMap{
                 {
                     object insideVal = ReadValue(json);
                     res.Add(insideVal);
-                }else{
+                }
+                else
+                {
                     idx++;
                 }
-                
+
             }
             idx++; //skipping ]
-            return new object[]{0};
+            return res.ToArray();
         }
+        
+        //parses string in json with in " -> "
         private string ParseString(string json)
         {
-            //parses string in json with in " -> "
             string buffer = "";
             if (json[idx] != '\"')
             {
@@ -204,13 +242,14 @@ namespace jsonMap{
             return buffer;
 
         }
+        
+        //skips white spaces
         private void SkipWhitespaces(string json)
         {
-            //skips white spaces
             while (json[idx] == ' ' && json.Length > idx)
             {
                 idx++;
             }
         }
-    }    
+    }
 }
